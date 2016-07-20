4413060aea36c736399e6afd1bd5564ba96ab02e
Leilani Battle <leibatt@gmail.com>
2016-07-20 13:36:00 -0400

    sorting by depth for line chart

--- web/station_events.js
+++ web/station_events.js
@@ -46,6 +46,7 @@ function addClickEventsForStations(stationSelector,linev,linevSelector) {
     getSpecificSampleDataForStation(stationId,defaultFields,function(res){
       console.log("query results:",res);
       // draw the line chart
+      res.sort(dl.comparator('depth_m'));
       linev.fields(defaultFields);
       linev.points(vega.changeset().remove(function(){return true;}).insert(res));
       //linev.points(vega.changeset().insert([{"depth_m":10,"nitrat_umol_per_kg":12.96},{"depth_m":10,"nitrat_umol_per_kg":13.33},{"depth_m":24,"nitrat_umol_per_kg":13.06},{"depth_m":25,"nitrat_umol_per_kg":13.04},{"depth_m":49,"nitrat_umol_per_kg":15.13},{"depth_m":51,"nitrat_umol_per_kg":13.86},{"depth_m":74,"nitrat_umol_per_kg":19.32},{"depth_m":75,"nitrat_umol_per_kg":20.54},{"depth_m":99,"nitrat_umol_per_kg":24.03},{"depth_m":100,"nitrat_umol_per_kg":22.51},{"depth_m":151,"nitrat_umol_per_kg":25.52},{"depth_m":151,"nitrat_umol_per_kg":25.76},{"depth_m":200,"nitrat_umol_per_kg":26.68},{"depth_m":200,"nitrat_umol_per_kg":26.65},{"depth_m":250,"nitrat_umol_per_kg":28.04},{"depth_m":250,"nitrat_umol_per_kg":27.62},{"depth_m":300,"nitrat_umol_per_kg":29.09},{"depth_m":399,"nitrat_umol_per_kg":30.4},{"depth_m":399,"nitrat_umol_per_kg":30.35},{"depth_m":400,"nitrat_umol_per_kg":30.77}]));
