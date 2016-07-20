242747cf6587434d0e1b302dcf3a0f796b2eb461
Leilani Battle <leibatt@gmail.com>
2016-07-20 16:13:01 -0400

    added checkboxes to add/remove lines

--- web/station_events.js
+++ web/station_events.js
@@ -1,3 +1,12 @@
+// for changes in radio buttons
+function changeMeasures() {
+  var clicked = $('.map .points circle.sampledata.emphasize')[0];
+  if(clicked) {
+    //$(clicked).removeClass('emphasize'); // skip the unclick step
+    $(clicked).trigger('click');
+  }
+};
+
 function addHoverEventsForStations(stationSelector) {
   $(stationSelector).off('mouseenter',stationSelector)
     .off('mouseleeave',stationSelector)
@@ -40,12 +49,14 @@ function getAggregateData(o,stationId){
 function addClickEventsForStations(stationSelector,linev,linevSelector) {
   $(stationSelector).off('click',stationSelector).on('click',function() {
     var clicked = $(this).hasClass('emphasize');
+/*
     if(clicked) {
       $('.map .points circle.sampledata').removeClass('emphasize');
       $('.map .points circle.sampledata').removeClass("deemphasize");
       $(linevSelector).addClass('hide');
       return;
     }
+*/
     $('.map .points circle.sampledata').removeClass('emphasize');
     $('.map .points circle.sampledata').addClass("deemphasize");
     $(this).addClass('emphasize');
@@ -53,12 +64,15 @@ function addClickEventsForStations(stationSelector,linev,linevSelector) {
     stationId = this.datum.bodc_station;
     // TODO: trigger a line chart to be drawn
     // get the necessary data for this station
-    var defaultFields = ['nitrat_umol_per_kg', 'silcat_umol_per_kg', 'phspht_umol_per_kg'];
-    getSpecificSampleDataForStation(stationId,defaultFields,function(res){
-      // console.log("query results:",res);
+    //var defaultFields = ['nitrat_umol_per_kg', 'silcat_umol_per_kg', 'phspht_umol_per_kg'];
+    var fields = [];
+    $("input[type='checkbox']:checked").map(function() {fields.push($(this).val());});
+    console.log("fields",fields);
+    getSpecificSampleDataForStation(stationId,fields,function(res){
+      //console.log("query results:",res);
       // draw the line chart
       res.sort(dl.comparator('depth_m'));
-      linev.fields(defaultFields);
+      linev.fields(fields);
       linev.points(vega.changeset().remove(function(){return true;}).insert(res));
       //linev.points(vega.changeset().insert([{"depth_m":10,"nitrat_umol_per_kg":12.96},{"depth_m":10,"nitrat_umol_per_kg":13.33},{"depth_m":24,"nitrat_umol_per_kg":13.06},{"depth_m":25,"nitrat_umol_per_kg":13.04},{"depth_m":49,"nitrat_umol_per_kg":15.13},{"depth_m":51,"nitrat_umol_per_kg":13.86},{"depth_m":74,"nitrat_umol_per_kg":19.32},{"depth_m":75,"nitrat_umol_per_kg":20.54},{"depth_m":99,"nitrat_umol_per_kg":24.03},{"depth_m":100,"nitrat_umol_per_kg":22.51},{"depth_m":151,"nitrat_umol_per_kg":25.52},{"depth_m":151,"nitrat_umol_per_kg":25.76},{"depth_m":200,"nitrat_umol_per_kg":26.68},{"depth_m":200,"nitrat_umol_per_kg":26.65},{"depth_m":250,"nitrat_umol_per_kg":28.04},{"depth_m":250,"nitrat_umol_per_kg":27.62},{"depth_m":300,"nitrat_umol_per_kg":29.09},{"depth_m":399,"nitrat_umol_per_kg":30.4},{"depth_m":399,"nitrat_umol_per_kg":30.35},{"depth_m":400,"nitrat_umol_per_kg":30.77}]));
       // display the line chart, if currently hidden
